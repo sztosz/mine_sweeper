58aaaf801a80c160ccd663d1d4f769bf268c675b
Bartosz Nowak <sztosz@gmail.com>
2020-08-29 09:56:37 +0200

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,80 @@
 use bevy::prelude::*;
 use bevy::render::camera::Camera;
-
+use rand::Rng;
 // const WIDTH: f32 = 40.0;
 // const HEIGHT: f32 = 40.0;
+const BOARD_SIZE: usize = 10;
 
 fn main() {
     App::build()
         .add_default_plugins()
         .init_resource::<State>()
-        .add_startup_system(setup.system())
+        .add_resource(create_board())
         .add_resource(Mouse {
             pos: Vec2::new(0.0, 0.0),
             world_pos: Vec2::new(0.0, 0.0),
             clicked: false,
         })
+        .add_startup_system(setup.system())
         .add_system(mouse_movement_updating_system.system())
         .add_system(click_system.system())
         .run();
 }
 
-struct MineField;
+struct Discovered(bool);
+struct Interactable(bool);
 
-fn setup(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
+struct X(usize);
+struct Y(usize);
+
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    board: Res<Board>,
+) {
     commands.spawn(Camera2dComponents::default());
 
-    let field_rows = 10;
-    let field_columns = 10;
     let field_spacing = 1.0;
     let field_size = Vec2::new(40.0, 40.0);
-    let field_width = field_columns as f32 * (field_size.x() + field_spacing) - field_spacing;
-    let field_height = field_columns as f32 * (field_size.y() + field_spacing) - field_spacing;
+    let field_width = BOARD_SIZE as f32 * (field_size.x() + field_spacing) - field_spacing;
+    let field_height = BOARD_SIZE as f32 * (field_size.y() + field_spacing) - field_spacing;
     let field_offset = Vec3::new(
         -(field_width - field_size.x()) / 2.0,
         -(field_height - field_size.x()) / 2.0,
         0.0,
     );
-    for row in 0..field_rows {
-        let y_position = row as f32 * (field_size.y() + field_spacing);
-        for column in 0..field_columns {
-            let field_position = Vec3::new(
-                column as f32 * (field_size.x() + field_spacing),
-                y_position,
-                0.0,
-            ) + field_offset;
 
-            commands
-                .spawn(SpriteComponents {
-                    material: materials.add(Color::rgb(0.2, 0.2, 0.2).into()),
-                    sprite: Sprite { size: field_size },
-                    translation: Translation(field_position),
-                    ..Default::default()
-                })
-                .with(MineField);
-        }
+    for (pos, tile) in board.revealed.iter().enumerate() {
+        let x_pos = (pos / BOARD_SIZE) as f32 * (field_size.y() + field_spacing);
+        let y_pos = (pos % BOARD_SIZE) as f32 * (field_size.y() + field_spacing);
+        let x = pos / BOARD_SIZE;
+        let y = pos % BOARD_SIZE;
+        let translation = Translation(Vec3::new(x_pos, y_pos, 0.0) + field_offset);
+        commands
+            .spawn(SpriteComponents {
+                material: materials.add(Color::rgba(0.2, 0.2, 0.2, 0.1).into()),
+                sprite: Sprite { size: field_size },
+                translation,
+                ..Default::default()
+            })
+            .with(TextComponents {
+                text: Text {
+                    value: format!("{}, {}", x, y),
+                    font: asset_server
+                        .load("assets/fonts/FiraMono-Medium.ttf")
+                        .unwrap(),
+                    style: TextStyle {
+                        font_size: 5.0,
+                        color: Color::WHITE,
+                    },
+                },
+                ..Default::default()
+            })
+            .with(Discovered(false))
+            .with(Interactable(true))
+            .with(X(x))
+            .with(Y(y));
     }
 }
 
@@ -92,15 +114,15 @@ fn mouse_movement_updating_system(
 fn click_system(
     mouse: ResMut<Mouse>,
     mut materials: ResMut<Assets<ColorMaterial>>,
-    mut minefield_query: Query<(
-        &MineField,
+    mut tile_query: Query<(
+        &Interactable,
         &Translation,
         &Sprite,
         &mut Handle<ColorMaterial>,
     )>,
 ) {
     if mouse.clicked {
-        for (mf, translation, sprite, color) in &mut minefield_query.iter() {
+        for (_, translation, sprite, color) in &mut tile_query.iter() {
             let position = translation.truncate();
             let extents = sprite.size / 2.0;
             let min = position - extents;
@@ -113,5 +135,43 @@ fn click_system(
                 }
             }
         }
+    }
+}
+
+#[derive(Default, Debug)]
+struct Board {
+    mines: Vec<bool>,
+    revealed: Vec<bool>,
+    nearby_mines: Vec<usize>,
+}
+
+fn create_board() -> Board {
+    let mut rng = rand::thread_rng();
+    let mut mines = vec![false; BOARD_SIZE * BOARD_SIZE];
+    let mut nearby_mines = vec![0; BOARD_SIZE * BOARD_SIZE];
+    let mut numbers: Vec<usize> = Vec::new();
+    let mut generated_count = 0;
+    loop {
+        let r = rng.gen_range(0, BOARD_SIZE * BOARD_SIZE);
+        if !numbers.contains(&r) {
+            numbers.push(r);
+            if let Some(elem) = mines.get_mut(r) {
+                *elem = true;
+            }
+            if let Some(elem) = nearby_mines.get_mut(r) {
+                *elem += 1;
+            }
+
+            generated_count = generated_count + 1;
+        }
+        if generated_count >= BOARD_SIZE {
+            break;
+        }
+    }
+    let revealed = vec![false; BOARD_SIZE * BOARD_SIZE];
+    Board {
+        mines,
+        revealed,
+        nearby_mines,
     }
 }
